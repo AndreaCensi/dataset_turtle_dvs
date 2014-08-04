7c0a16076974ef6d26bd7252c3f8702de2a5ea8b
Andrea Censi <andrea@cds.caltech.edu>
2014-08-04 16:13:37 -0700

    minor

--- src/dataset_turtle_dvs/__init__.py
+++ src/dataset_turtle_dvs/__init__.py
@@ -8,20 +8,14 @@ from .utils_images import *
 from .utils_imu import *
 
 
-
 def jobs_comptests(context):
-    from comptests import jobs_registrar
+    from conf_tools import GlobalConfig
+    GlobalConfig.global_load_dirs(['dataset_turtle_dvs.configs'])
 
-    # get testing configuration directory 
-    from pkg_resources import resource_filename  # @UnresolvedImport
-    dirname = resource_filename("dataset_turtle_dvs", "configs")
-    
-    # load into rawlogs config
-    from rawlogs import get_rawlogs_config
-    config = get_rawlogs_config()
-    config.load(dirname)
-    
     # Our tests are its tests with our configuration
     from rawlogs import unittests
-    j1 = jobs_registrar(context, config)
-    return j1
+
+    from comptests import jobs_registrar
+    from rawlogs import get_rawlogs_config
+    jobs_registrar(context, get_rawlogs_config())
+
